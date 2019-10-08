88c9261b8c7dc0a779c6371cf220380a270c6622
Antonin Herrero <herrero.antonin@gmail.com>
2019-10-08 13:39:48 +0200

    Save

---
+++ Assets/Scripts/Branch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch : MonoBehaviour
+{
+    SpriteRenderer spriteBranch;
+    List<GameObject> childs;
+    Branch parent;
+
+    int spawnX;
+    int spawnY;
+
+    int scale;
+    
+    void Start()
+    {
+        scale = 100;
+        spriteBranch = GetComponent<SpriteRenderer>();
+    }
+
+    public void AddChild(GameObject child)
+    {
+        childs.Add(child);
+    }
+}
---
+++ Assets/Scripts/CoralGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralGenerator : MonoBehaviour
+{
+    Transform coralRoot;
+    public GameObject coralPrefab;
+
+    int height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Generate()
+    {
+
+    }
+
+    void GenerateChilds(GameObject currentCoral, int currentHeight)
+    {
+        if (currentHeight < 1) {
+            GameObject childA = Instantiate(coralPrefab, currentCoral.transform);
+            childA.AddComponent<Branch>();
+            currentCoral.GetComponent<Branch>().AddChild(childA);
+
+            GameObject childB = Instantiate(coralPrefab, currentCoral.transform);
+            childB.AddComponent<Branch>();
+            currentCoral.GetComponent<Branch>().AddChild(childB);
+
+            GenerateChilds(currentHeight - 1);
+        }
+    }
+}
