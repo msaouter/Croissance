88c4f89214e6151a97ec8b27408b73ed646c460e
msaouter <msaouter@gmail.com>
2019-10-09 17:49:39 +0200

    Début intégration audio/graphique

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,9 +11,64 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID MUSIC_CONTEMPLATION = 2990463502U;
+        static const AkUniqueID MUSIC_DEGRADATION_0 = 1507659042U;
+        static const AkUniqueID MUSIC_DEGRADATION_1 = 1507659043U;
+        static const AkUniqueID MUSIC_DEGRADATION_2 = 1507659040U;
+        static const AkUniqueID MUSIC_DEGRADATION_3 = 1507659041U;
+        static const AkUniqueID MUSIC_DEGRADATION_4 = 1507659046U;
+        static const AkUniqueID MUSIC_KILL_CORAL = 41896295U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_DEGRADATION
+        {
+            static const AkUniqueID GROUP = 1532608259U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DEGRADATION_0 = 2065453594U;
+                static const AkUniqueID DEGRADATION_1 = 2065453595U;
+                static const AkUniqueID DEGRADATION_2 = 2065453592U;
+                static const AkUniqueID DEGRADATION_3 = 2065453593U;
+                static const AkUniqueID DEGRADATION_4 = 2065453598U;
+            } // namespace STATE
+        } // namespace MUSIC_DEGRADATION
+
+        namespace MUSIC_ST
+        {
+            static const AkUniqueID GROUP = 2996874600U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CONTEMPLATION = 3810549462U;
+                static const AkUniqueID KILL_CORAL = 981024095U;
+            } // namespace STATE
+        } // namespace MUSIC_ST
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_SIDECHAIN = 3336838409U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CROISSANCE = 2770033377U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID FX = 1802970371U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
