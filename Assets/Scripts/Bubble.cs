be4b8be33af6c70fe84098bce87f778c82b4bd30
msaouter <msaouter@gmail.com>
2019-10-10 11:24:47 +0200

    Intégration génération bulles

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public float speed;
+    public float xVariationSpread;
+    public float scaleVariation;
+    public float MaxHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y>MaxHeight)
+            Destroy(this.gameObject);
+        float var = Random.Range(-scaleVariation, scaleVariation);
+        this.transform.position += new Vector3(Random.Range(-xVariationSpread,xVariationSpread), speed,0)* Time.deltaTime;
+        this.transform.localScale += new Vector3(var, var, 0) * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/BubblesGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesGenerator : MonoBehaviour
+{
+
+    public List<GameObject> bubble;
+    public int randomIndex;
+    public float averageSecondsBetweenBubbles;
+    public float BubbleStartingPositionRange;
+    public float bubbleSpeed;
+    public float spreadBubbleSpeed;
+    public float bubbleXVariationSpread;
+    public float bubbleScaleVariation;
+    public float ScaleVariationSpreadRange;
+    public float MaxHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomIndex = Random.Range(0, bubble.Capacity);
+        bubble[randomIndex].GetComponent<Bubble>().MaxHeight = this.MaxHeight;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(randomIndex);
+
+        if (Random.Range(0f,1f)<(1/averageSecondsBetweenBubbles * Time.deltaTime))
+        {           
+            bubble[randomIndex].GetComponent<Bubble>().speed = bubbleSpeed+ Random.Range(-spreadBubbleSpeed,spreadBubbleSpeed);
+            bubble[randomIndex].GetComponent<Bubble>().xVariationSpread = bubbleXVariationSpread;
+            bubble[randomIndex].GetComponent<Bubble>().scaleVariation = bubbleScaleVariation + Random.Range(-ScaleVariationSpreadRange, ScaleVariationSpreadRange);
+            
+            GameObject randomBubble = Instantiate<GameObject>(bubble[randomIndex], this.transform.position + new Vector3(Random.Range(-BubbleStartingPositionRange, BubbleStartingPositionRange),
+                Random.Range(-BubbleStartingPositionRange, BubbleStartingPositionRange), Random.Range(-BubbleStartingPositionRange, BubbleStartingPositionRange)),
+                Camera.main.transform.rotation);
+
+            randomIndex = Random.Range(0, bubble.Capacity);
+        }
+
+    }
+}
